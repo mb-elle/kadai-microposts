08233348855e7f1bcc33fc19424ecfc16c30c7cd
m.b <m.9745839.b@gmail.com>
2020-07-01 02:46:29 +0000

    follow, unfollow

--- resources/views/users/followers.blade.php
+++ resources/views/users/followers.blade.php
@@ -5,14 +5,11 @@
         <aside class="col-sm-4">
             @include('users.card')
         </aside>
+        
         <div class="col-sm-8">
             @include('users.navtabs')
             
-            @if (Auth::id() == $user->id)
-                @include('microposts.form')
-            @endif
-            
-            @include('microposts.microposts')
+            @include('users.users')
         </div>
     </div>
 @endsection
